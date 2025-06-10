b429eb57f1b0fd7e9f123b01e4bf08d42f76b533
quim-romero <quimromero22@gmail.com>
2025-06-10 11:10:00 +0200

    feat: add routing between Home, Login, Onboarding and Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Onboarding from "./pages/Onboarding"
+import Dashboard from "./pages/Dashboard"
 
 export default function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<div>Home</div>} />
-      </Routes>
-    </>
-  );
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/onboarding" element={<Onboarding />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  )
 }
