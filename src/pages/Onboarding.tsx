d8202fa2f296bd00fe4ad52e4327a51b0844f1e2
quim-romero <quimromero22@gmail.com>
2025-06-15 15:00:00 +0200

    feat: add Step3 to onboarding flow

--- src/pages/Onboarding.tsx
+++ src/pages/Onboarding.tsx
@@ -2,8 +2,10 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import StepIndicator from "../components/StepIndicator";
 import { useOnboardingStore } from "../store/onboardingStore";
+
 import Step1 from "../features/onboarding/Step1";
 import Step2 from "../features/onboarding/Step2";
+import Step3 from "../features/onboarding/Step3";
 
 export default function Onboarding() {
   const user = useOnboardingStore((s) => s.user);
@@ -19,7 +21,8 @@ export default function Onboarding() {
   return (
     <main className="min-h-screen py-12">
       {step < 5 && <StepIndicator />}
-      {step === 1 && <Step1 />} {step === 2 && <Step2 />}
+      {step === 1 && <Step1 />} {step === 2 && <Step2 />}{" "}
+      {step === 3 && <Step3 />}
     </main>
   );
 }
