e3c35d258f355ab85932c01083f14aa2aa2ca7ff
quim-romero <quimromero22@gmail.com>
2025-07-24 10:00:00 +0200

    perf(lighthouse): self-host fonts + add favicons; drop Google Fonts, fix render-blocking CSS and /favicon.ico 404 to improve LCP/Best Practices

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
-import App from "./App"
-import "./styles/globals.css"
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/unbounded/600.css";
+import "@fontsource/unbounded/700.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -10,4 +16,4 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <App />
     </BrowserRouter>
   </React.StrictMode>
-)
+);
